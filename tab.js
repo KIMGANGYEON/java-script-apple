a6a9eb71df00a0ac92ed078fdc23eb6031509fc4
KIMGANGYEON <rkddus3177@naver.com>
2024-01-23 22:08:59 +0900

    That's it for today

--- tab.js
+++ tab.js
@@ -1,17 +1,43 @@
 var tabbutton = $('.tab-button');
 var tabcontent = $('.tab-content')
 
+
+
+// (html 데이터 추가 방식)
+$('.list').click(function(e){
+    console.log(e.target.dataset.id);
+   tabs(parseInt(e.target.dataset.id));
+
+})
+
+
 function tabs(num){
     tabbutton.removeClass('orange');
     tabbutton.eq(num).addClass('orange');
     tabcontent.removeClass('show');
     tabcontent.eq(num).addClass('show');
-    
 }
-// for (let i = 0; i < tabbutton.length; i++){
-//     tabbutton.eq(i).on('click', function(){
+
+// for(let i = 0;  i < 3; i++){
+//     $('.tab-button').eq(i).on('click', function(){
 //         tabs(i)
 //     })
+// }
+// function tabs(num){
+//     tabbutton.removeClass('orange');
+//     tabbutton.eq(num).addClass('orange');
+//     tabcontent.removeClass('show');
+//     tabcontent.eq(num).addClass('show');
+// }
+
+
+// for (let i = 0; i < tabbutton.length; i++){
+//     tabbutton.eq(i).on('click', function(){
+//         tabbutton.removeClass('orange');
+//         tabbutton.eq(i).addClass('orange');
+//         tabcontent.removeClass('show');
+//         tabcontent.eq(i).addClass('show');
+//     });
 // }
 
 // var tabbutton = $('.tab-button');
